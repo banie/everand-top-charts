9d2537e856bc85e81157ccdf06990ee53da63964
banie <banie.setijoso@gmail.com>
2024-04-14 01:02:25 -0400

    Finally have the divider below the nav title that would span the whole width without that annoying inset on the start

--- everandTopCharts/everandTopCharts/ContentView.swift
+++ everandTopCharts/everandTopCharts/ContentView.swift
@@ -14,41 +14,44 @@ struct ContentView: View {
 
     var body: some View {
         NavigationSplitView {
-            List {
-                Text("The most popular books and audiobooks generating buzz from critics, NYT and more.")
-                    .foregroundColor(.secondary)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                ForEach(items.indices, id: \.self) { index in
-                    HStack {
-                        Text("\(index + 1). ")
-                            .frame(maxHeight: .infinity, alignment: .top)
-                            .padding(.top)
-                        Image(systemName: "book.closed.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(maxWidth: 50, maxHeight: 50)
-                        VStack(spacing: 2) {
-                            Text("Book Title")
-                                .font(.title2)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text("Book Author")
-                                .font(.subheadline)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            HStack(spacing: 2) {
-                                let numOfStars: Int = 4
-                                ForEach(1...numOfStars, id: \.self) { _ in
-                                    Image(systemName: "star.fill")
+            VStack {
+                Divider()
+                List {
+                    Text("The most popular books and audiobooks generating buzz from critics, NYT and more.")
+                        .foregroundColor(.secondary)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    ForEach(items.indices, id: \.self) { index in
+                        HStack {
+                            Text("\(index + 1). ")
+                                .frame(maxHeight: .infinity, alignment: .top)
+                                .padding(.top)
+                            Image(systemName: "book.closed.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: 50, maxHeight: 50)
+                            VStack(spacing: 2) {
+                                Text("Book Title")
+                                    .font(.title2)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text("Book Author")
+                                    .font(.subheadline)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                HStack(spacing: 2) {
+                                    let numOfStars: Int = 4
+                                    ForEach(1...numOfStars, id: \.self) { _ in
+                                        Image(systemName: "star.fill")
+                                    }
                                 }
+                                .padding(.top, 5)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .foregroundColor(.yellow)
                             }
-                            .padding(.top, 5)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.yellow)
+                            .padding(.leading, 10)
                         }
-                        .padding(.leading, 10)
+                        .listRowSeparator(.hidden)
                     }
-                    .listRowSeparator(.hidden)
+                    .onDelete(perform: deleteItems)
                 }
-                .onDelete(perform: deleteItems)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
