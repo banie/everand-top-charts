3aaec2c38c31ad8f6d42b2ec9ebbab6103df8913
banie <banie.setijoso@gmail.com>
2024-04-14 13:42:11 -0400

    Implement the interactor logic to get the list of books

---
+++ everandTopCharts/everandTopCharts/ViewModel/Interactor/GetBookList.swift
@@ -0,0 +1,30 @@
+//
+//  GetCarListApi.swift
+//  Guidomia
+//
+//  Created by Banie Setijoso on 2023-02-03.
+//
+
+import Foundation
+
+public enum ApiError: Error {
+    case invalidPath
+    case invalidUrl
+    case parsingError
+}
+
+protocol GetBooksApi {
+    func getListOfBooks() -> Result<[Book], ApiError>
+}
+
+class GetBooksFromResource: GetBooksApi {
+    private let getRequestApi: GetFromLocalResource
+    
+    init(getRequestApi: GetFromLocalResource = GetFromLocalResource()) {
+        self.getRequestApi = getRequestApi
+    }
+    
+    func getListOfBooks() -> Result<[Book], ApiError> {
+        return getRequestApi.get(from: "book_list.json")
+    }
+}
---
+++ everandTopCharts/everandTopCharts/ViewModel/Interactor/GetFromLocalResource.swift
@@ -0,0 +1,33 @@
+//
+//  GetFromLocalResource.swift
+//  Guidomia
+//
+//  Created by Banie Setijoso on 2023-02-03.
+//
+
+import Foundation
+
+class GetFromLocalResource {
+    let decoder: JSONDecoder
+    
+    init() {
+        decoder = JSONDecoder()
+    }
+    
+    func get<T>(from fileName: String) -> Result<T, ApiError> where T: Decodable {
+        guard let path = Bundle.main.path(forResource: fileName, ofType: "json") else {
+            return .failure(.invalidPath)
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let items = try decoder.decode(T.self, from: data)
+            return .success(items)
+        } catch {
+            print("not able to parse the items from filePath: \(fileName), error: \(error)")
+            return .failure(.parsingError)
+        }
+    }
+}
